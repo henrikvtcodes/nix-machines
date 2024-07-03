1bb78907593dbcb6483cb8df410407569a947475
henrikvtcodes <commits@henrikvt.com>
2024-07-03 19:07:36 -0400

    the false hope of adding stuff

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
       url = "github:nixos/nixpkgs/release-24.05";
     };
 
+    systems.url = "github:nix-systems/default";
+
     # Core tools: home manager, secrets, disk partitioning, deployment
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -13,7 +15,8 @@
     };
 
     agenix = {
-      url = "github:ryantm/agenix";
+      # agenix-compatible but in rust, for stability
+      url = "github:yaxitech/ragenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -25,6 +28,10 @@
     deploy-rs = {
       url = "github:serokell/deploy-rs";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    impermanence = {
+      url = "github:nix-community/impermanence";
     };
 
     # https://docs.hercules-ci.com/arion/
@@ -46,6 +53,8 @@
       deploy-rs,
       home-manager,
       agenix,
+      systems,
+      disko,
       ...
     }@inputs:
     let
@@ -79,8 +88,54 @@
             # Machine config
             ./machines/nixos
             ./machines/nixos/svalbard
-
+            disko.nixosModules.disko
             agenix.nixosModules.default
+
+            # Services config
+
+            # User config
+            ./users
+            home-manager.nixosModules.home-manager.default
+            {
+              home-manager = {
+                users.henrikvt = import ./users/henrikvt;
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                extraSpecialArgs = {
+                  inherit inputs vars;
+                };
+              };
+            }
+          ];
+        };
+
+        doghouse = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = {
+            inherit inputs vars;
+          };
+          modules = [
+            # Machine config
+            ./machines/nixos
+            ./machines/nixos/doghouse
+            disko.nixosModules.disko
+            agenix.nixosModules.default
+
+            # Services config
+
+            # User config
+            ./users
+            home-manager.nixosModules.home-manager.default
+            {
+              home-manager = {
+                users.henrikvt = import ./users/henrikvt;
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                extraSpecialArgs = {
+                  inherit inputs vars;
+                };
+              };
+            }
           ];
         };
       };
@@ -93,6 +148,16 @@
             path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.svalbard;
           };
         };
+        doghouse = {
+          hostName = "doghouse";
+          profiles.system = {
+            remoteBuild = true;
+            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.doghouse;
+          };
+        };
       };
+
+      # Enables nix flake check to ensure that the deploy-rs config is correct
+      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
     };
 }
