65dc28e3d4f4317ce850c24507b215e57f2ed6fd
henrikvtcodes <commits@henrikvt.com>
2024-07-08 21:05:04 -0400

    set identityPaths for (r)agenix

--- secrets/default.nix
+++ secrets/default.nix
@@ -5,6 +5,8 @@
   ...
 }:
 {
+  age.identityPaths = lib.mkDefault [ "~/.ssh/id_ed25519" ];
+
   age.secrets.tailscaleAuthKey.file = ./tailscaleAuthKey.age;
   age.secrets.henrikUserPassword.file = ./henrikUserPassword.age;
 }
