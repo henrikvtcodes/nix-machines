46ec835be95d74df49f53b639b31d570e2a34538
henrikvtcodes <commits@henrikvt.com>
2024-07-29 16:18:40 -0400

    create base config for the new `doghouse` machine

---
+++ machines/nixos/doghouse/default.nix
@@ -0,0 +1,37 @@
+{
+  inputs,
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  imports = [
+    # Include the results of the hardware scan.
+    # ./hardware-config.nix
+    # ./disk-config.nix
+  ];
+
+  networking.hostName = "doghouse";
+  # networking.hostId = "";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  bootDisk = {
+    enable = true;
+    # diskPath = "";
+  };
+
+  networking.firewall.enable = false;
+  networking.wireless.enable = false;
+  networking.networkmanager.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05";
+}
