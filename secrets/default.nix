f529c9bff7093a1b30b4f8ac07d161f0db8cc208
henrikvtcodes <commits@henrikvt.com>
2024-07-29 18:07:18 -0400

    add donso pubkey && briefly allow donso to access my user password

--- secrets/default.nix
+++ secrets/default.nix
@@ -7,4 +7,9 @@
 {
   age.secrets.tailscaleAuthKey.file = ./tailscaleAuthKey.age;
   age.secrets.henrikUserPassword.file = ./henrikUserPassword.age;
+
+  age.identityPaths = [
+    "/etc/ssh/ssh_host_ed25519_key"
+    "/etc/ssh/ssh_host_rsa_key"
+  ];
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,18 @@
 let
+
+  # --------- Henrik's Public Keys ---------
   # Fetch public key from https://henrikvt.com/id_ed25519.pub
   henrik_public_pubkey = builtins.readFile (builtins.fetchurl "https://henrikvt.com/id_ed25519.pub");
-
   # Fetch git signing key from https://github.com/henrikvtcodes.keys
   henrik_git_pubkey = builtins.readFile (builtins.fetchurl "https://github.com/henrikvtcodes.keys");
-
   henrik_homelab_pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMmZEhaLdiFJ6TdyhdBC5fvCiY5c7drQK2EVHGPCPHei";
 
+  # --------- Systems ---------
   svalbard = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIApAfSOQUWsDCYUKaPHxO8j4Wo9xGBSlvASMRIrNLYtD";
 
-  doghouse = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUWyXkH30owx5qEz8gi6QjbPTEad2xzN7iVRO5knY8Q root@nixos";
+  valcour = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUWyXkH30owx5qEz8gi6QjbPTEad2xzN7iVRO5knY8Q root@nixos";
+
+  donso = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL7mee8J7n3mxPJqkkNoKwMeez9ezUSEa0+a2h3WDGAD";
 
   henrik = [
     henrik_public_pubkey
@@ -21,11 +24,12 @@ let
 
   systems = [
     svalbard
-    doghouse
+    valcour
+    donso
   ];
 in
 {
   "tailscaleAuthKey.age".publicKeys = users ++ systems;
 
-  "henrikUserPassword.age".publicKeys = henrik;
+  "henrikUserPassword.age".publicKeys = henrik ++ [ donso ];
 }
