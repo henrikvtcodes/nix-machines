34fc21eb76c8775510b15e428b969891f222a938
henrikvtcodes <commits@henrikvt.com>
2024-07-30 09:53:38 -0400

    try a different idea for the init script

--- init/init.sh
+++ init/init.sh
@@ -1,21 +1,22 @@
 # !/usr/bin/env bash
-mkdir -p /mnt/etc/nixos
+sleep 2
 
+sudo mkdir -p /mnt/etc/nixos
 # Generate the base config
-nixos-generate-config --no-filesystems --root /mnt
+sudo nixos-generate-config --no-filesystems --root /mnt
 
 # Download the disko configuration to /mnt/etc/nixos
-curl -o /mnt/etc/nixos/disk-config.nix https://raw.githubusercontent.com/henrikvtcodes/nix-machines/main/init/disk-config.nix
-# Prompt the user for which drive to install to
-echo "Which drive would you like to install to?"
-lsblk
-read -p DRIVE
+sudo curl -o /mnt/etc/nixos/disk-config.nix https://raw.githubusercontent.com/henrikvtcodes/nix-machines/main/init/disk-config.nix
+
+# Prompt the user for which drive to install to:
+echo "Which drive would you like to install to? (e.g. /dev/sda)"
+read -p "Drive: " DRIVE
 
 # Set the drive in the configuration
-sed -i "s|to-be-filled-during-installation|/dev/$DRIVE|" /mnt/etc/nixos/disk-config.nix
+sudo sed -i "s|to-be-filled-during-installation|$DRIVE|" /mnt/etc/nixos/disk-config.nix
 
 # Overwrite the configuration.nix with the provided configuration
-curl -o /mnt/etc/nixos/configuration.nix https://raw.githubusercontent.com/henrikvtcodes/nix-machines/main/init/configuration.nix
+sudo curl -o /mnt/etc/nixos/configuration.nix https://raw.githubusercontent.com/henrikvtcodes/nix-machines/main/init/configuration.nix
 
 # Finish by letting the user know what to do next
 echo "Script completed"
