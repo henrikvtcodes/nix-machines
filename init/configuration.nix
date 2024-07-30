40150ac0617aa2a718e2417efb44b5ecbee5fef6
henrikvtcodes <commits@henrikvt.com>
2024-07-30 10:03:32 -0400

    make sure disko is like, actually present in the init thing

--- init/configuration.nix
+++ init/configuration.nix
@@ -8,6 +8,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
     ./disk-config.nix
   ];
 
--- init/init.sh
+++ init/init.sh
@@ -1,4 +1,4 @@
-# !/usr/bin/env bash
+# !/bin/bash
 sleep 2
 
 sudo mkdir -p /mnt/etc/nixos
@@ -9,12 +9,12 @@ sudo nixos-generate-config --no-filesystems --root /mnt
 rm -vf /mnt/etc/nixos/disk-config.nix
 sudo curl -o /mnt/etc/nixos/disk-config.nix https://raw.githubusercontent.com/henrikvtcodes/nix-machines/main/init/disk-config.nix
 
-# Prompt the user for which drive to install to:
-echo "Which drive would you like to install to? (e.g. /dev/sda)"
-read -p "Drive: " DRIVE
+# # Prompt the user for which drive to install to:
+# echo "Which drive would you like to install to? (e.g. /dev/sda)"
+# read -p "Drive: " DRIVE
 
-# Set the drive in the configuration
-sudo sed -i "s|to-be-filled-during-installation|$DRIVE|" /mnt/etc/nixos/disk-config.nix
+# # Set the drive in the configuration
+# sudo sed -i "s|to-be-filled-during-installation|$DRIVE|" /mnt/etc/nixos/disk-config.nix
 
 # Overwrite the configuration.nix with the provided configuration
 rm -vf /mnt/etc/nixos/configuration.nix
@@ -22,4 +22,5 @@ sudo curl -o /mnt/etc/nixos/configuration.nix https://raw.githubusercontent.com/
 
 # Finish by letting the user know what to do next
 echo "Script completed"
+echo "Make sure to check /mnt/etc/nixos/disk-config.nix to ensure the correct device is set"
 echo "Next, run sudo nixos-install --root /mnt"
