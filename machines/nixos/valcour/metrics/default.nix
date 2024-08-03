ce77268ffd844c561b3129be99b14cb38e6c4441
henrikvtcodes <commits@henrikvt.com>
2024-08-02 22:11:47 -0500

    enable node exporter

--- machines/nixos/valcour/metrics/default.nix
+++ machines/nixos/valcour/metrics/default.nix
@@ -2,15 +2,23 @@
 {
   services.prometheus = {
     enable = true;
+    exporters.node.enable = true;
     scrapeConfigs = [
       {
         job_name = "BGP.Tools - AS215207";
-        scrape_interval = "15m";
+        scrape_interval = "1m";
         scheme = "https";
         metrics_path = "/prom/9bd7e9e4-f70a-48a9-a9a1-06396db1801a";
         static_configs = [ { targets = [ "bgp.tools" ]; } ];
       }
+
+      {
+        job_name = "Local Node Exporter";
+        scrape_interval = "15s";
+        static_configs = [ { targets = [ "localhost:9100" ]; } ];
+      }
     ];
+
   };
 
   services.grafana = {
