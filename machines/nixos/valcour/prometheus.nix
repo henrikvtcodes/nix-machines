aa25c6c62f4f9cd44b687153471f2a73eaab98f0
henrikvtcodes <commits@henrikvt.com>
2024-08-02 20:25:34 -0500

    fix type error in prometheus config

--- machines/nixos/valcour/prometheus.nix
+++ machines/nixos/valcour/prometheus.nix
@@ -2,12 +2,14 @@
 {
   services.prometheus = {
     enable = true;
-    scrapeConfigs."as215207-bgptools" = {
-      job_name = "BGP.Tools - AS215207";
-      scrape_interval = "15m";
-      scheme = "https";
-      metrics_path = "/prom/9bd7e9e4-f70a-48a9-a9a1-06396db1801a";
-      static_configs = [ { targets = [ "bgp.tools" ]; } ];
-    };
+    scrapeConfigs = [
+      {
+        job_name = "BGP.Tools - AS215207";
+        scrape_interval = "15m";
+        scheme = "https";
+        metrics_path = "/prom/9bd7e9e4-f70a-48a9-a9a1-06396db1801a";
+        static_configs = [ { targets = [ "bgp.tools" ]; } ];
+      }
+    ];
   };
 }
