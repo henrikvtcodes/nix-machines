ecbbcc00ab31258905f3d537071d6bf2e1653b6a
henrikvtcodes <commits@henrikvt.com>
2024-09-10 16:29:07 -0400

    rough config for metrics stuff

---
+++ modules/metrics/prometheus.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.metrics.prometheus;
+in
+{
+  options.metrics.prometheus = with lib; {
+    enable = mkEnableOption "Enable Prometheus";
+    retentionTime = mkOption {
+      type = types.str;
+      default = "60d";
+      description = ''
+        How long to keep metrics data.
+      '';
+    };
+    scrapeConfigs = mkOption {
+      type = types.listOf types.attrs;
+      default = [ ];
+      description = ''
+        List of scrape configurations.
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ prometheus ];
+    services.prometheus = {
+      enable = true;
+      retentionTime = cfg.retentionTime;
+      scrapeConfigs = cfg.scrapeConfigs;
+    };
+  };
+}
