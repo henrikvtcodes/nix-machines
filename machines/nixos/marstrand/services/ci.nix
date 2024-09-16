1e086842ce51119bbeeec3c73191da164de05ef5
henrikvtcodes <commits@henrikvt.com>
2024-09-16 11:29:36 -0400

    slowly getting ci agent to work on marstrand

--- machines/nixos/marstrand/services/ci.nix
+++ machines/nixos/marstrand/services/ci.nix
@@ -1,16 +1,63 @@
 { config, ... }:
 {
   # This sets up a woodpecker agent
-  services.woodpecker-agents.agents."marstrand" = {
+  services.woodpecker-agents.agents."docker" = {
     enable = true;
     # We need this to talk to the podman socket
-    extraGroups = [ "podman" ];
+    extraGroups = [
+      "podman"
+      "woodpecker"
+    ];
     environment = {
-      WOODPECKER_SERVER = "barnegat:9000";
+      WOODPECKER_SERVER = "barnegat:3006";
       WOODPECKER_MAX_WORKFLOWS = "4";
       DOCKER_HOST = "unix:///run/podman/podman.sock";
       WOODPECKER_BACKEND = "docker";
+      WOODPECKER_HEALTHCHECK = "true";
+      WOODPECKER_HEALTHCHECK_ADDR = "0.0.0.0:3007";
     };
     environmentFile = [ config.age.secrets.ciAgentSecrets.path ];
+  };
+
+  users = {
+    users.woodpecker = {
+      isSystemUser = true;
+      group = "woodpecker";
+      createHome = true;
+      home = "/etc/woodpecker";
+    };
+    groups.woodpecker = { };
+  };
+
+  systemd.services.ci-agent-config = {
+    wantedBy = [ "woodpecker-agent-docker.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      User = "woodpecker";
+      Group = "woodpecker";
+    };
+
+    script = ''
+      #!/bin/sh
+      set -e
+      mkdir -p /etc/woodpecker
+      if [ ! -f /etc/woodpecker/agent.conf ]; then
+        touch /etc/woodpecker/agent.conf
+      fi
+    '';
+  };
+
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    dockerSocket.enable = true;
+    defaultNetwork.settings = {
+      dns_enabled = true;
+    };
+  };
+
+  networking.firewall.interfaces."podman0" = {
+    allowedUDPPorts = [ 53 ];
+    allowedTCPPorts = [ 53 ];
   };
 }
