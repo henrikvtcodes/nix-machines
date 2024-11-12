d8cedfbdb60a6f514298e800c13b6932b4370967
henrikvtcodes <commits@henrikvt.com>
2024-11-12 12:07:54 -0500

    remove unused

--- machines/nixos/default.nix
+++ machines/nixos/default.nix
@@ -87,8 +87,6 @@
   time.timeZone = lib.mkDefault "America/New_York";
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # users.mutableUsers = false;
-
   # Clean /tmp on reboot
   boot.tmp.cleanOnBoot = true;
 
