13a6e7e8e0c6028b6655a855b60c1d2f94757030
Henrik VT <commits@henrikvt.com>
2025-01-28 22:43:32 -0500

    another idea

--- home/henrikvt/.zprofile.zsh
+++ home/henrikvt/.zprofile.zsh
@@ -21,8 +21,9 @@ alias pmid='ceiling_divide $LINES 2; clear_rows $ceiling_result'
 # Clear 3/4th
 alias pdown='ceiling_divide $((3*LINES)) 4; clear_rows $ceiling_result'
 
-alias ghurl='gh repo view --json sshUrl --jq .sshUrl $1 | cat'
-
 if command -v "gh" &> /dev/null; then
-  alias ghurl='gh repo view --json sshUrl --jq .sshUrl $1 | cat'
+  # alias ghurl='gh repo view --json sshUrl --jq .sshUrl $1 | cat'
+  function ghurl() {
+	  gh repo view --json sshUrl --jq .sshUrl $1 | cat
+  }
 fi
