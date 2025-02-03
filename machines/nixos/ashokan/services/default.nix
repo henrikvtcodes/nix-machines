5f3c9d069436f3b490561101cb7fdfc3d4bac159
Henrik VT <commits@henrikvt.com>
2025-02-03 11:25:41 -0500

    mastodon deploy works! no working interface yet

--- machines/nixos/ashokan/services/default.nix
+++ machines/nixos/ashokan/services/default.nix
@@ -3,6 +3,17 @@
 
   svcs.traefik = {
     enable = true;
+    logLevel = "DEBUG";
     environmentFiles = [config.age.secrets.cfDnsApiToken.path];
+  };
+
+  svcs.mastodon = {
+    enable = true;
+    configureTraefik = true;
+    secretKeyBaseEnvFile = config.age.secrets.mastodonSecretKeyBase.path;
+    otpSecretEnvFile = config.age.secrets.mastodonOtpSecret.path;
+    vapidKeysEnvFile = config.age.secrets.mastodonVapidKeys.path;
+    smtpPasswordEnvFile = config.age.secrets.mastodonSmtpPassword.path;
+    activeRecordEncryptionEnvFile = config.age.secrets.mastodonAREncryptionEnvVars.path;
   };
 }
