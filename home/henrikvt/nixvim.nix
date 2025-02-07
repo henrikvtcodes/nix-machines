11c75051bb0e6cbf6caab5a29c3f84a1e5611862
Henrik VT <commits@henrikvt.com>
2025-02-07 11:11:34 -0500

    more nvim/nixvim plugins

--- home/henrikvt/nixvim.nix
+++ home/henrikvt/nixvim.nix
@@ -8,8 +8,63 @@
     background.light = "mocha";
   };
 
-  # Javascript/Typescript
   plugins = {
+    #  ----------------
+    # | General Stuff |
+    #  ----------------
+    gitsigns = {
+      enable = true;
+      settings = {
+        current_line_blame = true;
+        current_line_blame_opts = {
+          virt_text = true;
+          virt_text_pos = "eol";
+        };
+        signcolumn = true;
+        signs = {
+          add = {text = "│";};
+          change = {text = "│";};
+          changedelete = {text = "~";};
+          delete = {text = "_";};
+          topdelete = {text = "‾";};
+          untracked = {text = "┆";};
+        };
+        watch_gitdir = {follow_files = true;};
+      };
+    };
+
+    hop.enable = true;
+    illuminate.enable = true;
+
+    nvim-lightbulb.enable = true;
+    lualine = {
+      enable = true;
+      settings.options.globalstatus = true;
+    };
+
+    luasnip.enable = true;
+    neo-tree.enable = true;
+
+    nvim-autopairs.enable = true;
+    # colorizer.enable = true;
+
+    render-markdown.enable = true;
+    todo-comments.enable = true;
+
+    treesitter = {
+      enable = true;
+
+      folding = false;
+      settings.indent.enable = true;
+    };
+
+    vim-surround.enable = true;
+    web-devicons.enable = true;
+
+    #  ------
+    # | LSP |
+    # ------
+    # Javascript/Typescript
     conform-nvim = {
       settings = {
         formatters_by_ft.javascript = ["prettier"];
