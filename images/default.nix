8cf64f1028c4a2d1209305c164b15fdfed4ef59b
Henrik VT <commits@henrikvt.com>
2025-03-28 10:00:37 -0400

    iso networking settings

--- images/default.nix
+++ images/default.nix
@@ -35,4 +35,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINimhbJZN+MLdXbtk3Mrb5dca7P+LKy399OqqYZ122Ml"
     ];
   };
+
+  networking.nameservers = ["1.0.0.1" "1.1.1.1" "2606:4700:4700::1111" "2606:4700:4700::1001"];
+  networking.networkmanager.enable = true;
 }
