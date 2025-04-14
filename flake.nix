5cb76a4be04b35be7d6ac174da50cc614a8086ec
Henrik VT <commits@henrikvt.com>
2025-04-13 22:31:16 -0400

    move to home manager unstablke

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
