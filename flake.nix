0b67a307bded534d8622d0dd1fe44b13895a583a
Henrik VT <commits@henrikvt.com>
2025-04-15 17:54:49 -0400

    swap home-manager back to nixpkgs stable

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
