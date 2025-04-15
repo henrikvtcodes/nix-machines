ac102ecfe6286669b6cdd3680fc3fc348711b037
Henrik VT <commits@henrikvt.com>
2025-04-15 17:42:19 -0400

    add public binary caches

--- machines/nixos/default.nix
+++ machines/nixos/default.nix
@@ -24,6 +24,11 @@
       ];
       system-features = ["recursive-nix"];
       trusted-users = ["root" "@wheel"];
+      extra-substituters = ["https://cache.garnix.io" "https://nix-community.cachix.org"];
+      extra-trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      ];
     };
   };
 
