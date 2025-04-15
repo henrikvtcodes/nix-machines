ea141fa4027da518f312c973565d1b9c5fd8c597
Henrik VT <commits@henrikvt.com>
2025-04-15 10:41:02 -0400

    add new machine (penikese)

---
+++ machines/nixos/penikese/default.nix
@@ -0,0 +1,19 @@
+{...}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-config.nix
+  ];
+
+  networking.hostName = "penikese";
+  networking.hostId = "e55b3488";
+
+  services.qemuGuest.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11";
+}
---
+++ machines/nixos/penikese/hardware-config.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+
+  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = [];
+  boot.extraModulePackages = [];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/61e3107a-9e43-44db-871d-1016a7319cce";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/A451-F3E6";
+    fsType = "vfat";
+    options = ["fmask=0077" "dmask=0077"];
+  };
+
+  swapDevices = [];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
