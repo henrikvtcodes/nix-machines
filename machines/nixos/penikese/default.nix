0b7fffaf9dbe630364416afe7a2ed8ea514dca84
Henrik VT <commits@henrikvt.com>
2025-04-15 11:10:10 -0400

    fixup! more stuff for penikese

--- machines/nixos/penikese/default.nix
+++ machines/nixos/penikese/default.nix
@@ -16,6 +16,10 @@
 
   users.henrikvt.enablePasswordFile = false;
 
+  networking = {
+    dhcpcd.IPv6rs = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
