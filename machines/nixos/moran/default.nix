4147e851a7e4ab290d33b88232295197640b407a
Henrik VT <commits@henrikvt.com>
2025-04-18 15:26:02 -0400

    add hyprland cache

--- machines/nixos/moran/default.nix
+++ machines/nixos/moran/default.nix
@@ -43,6 +43,15 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  nix = {
+    settings = {
+      extra-substituters = ["https://hyprland.cachix.org"];
+      extra-trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
+    };
+  };
+
   # fonts.packages = with pkgs; [ nerdfonts ];
 
   # Enable the X11 windowing system.
