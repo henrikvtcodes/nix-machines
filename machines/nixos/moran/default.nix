1f3fa7c3de8b1162574a81a76e8960c3f8d7bd2f
Henrik VT <commits@henrikvt.com>
2025-04-18 01:05:22 -0400

    boot loader setting

--- machines/nixos/moran/default.nix
+++ machines/nixos/moran/default.nix
@@ -11,6 +11,7 @@
   # boot.loader.systemd-boot.enable = true;
   boot.loader = {
     efi.canTouchEfiVariables = true;
+    efi.efiSysMountPoint = "/boot/efi";			
     grub = {
       enable = true;
       devices = ["nodev"];
