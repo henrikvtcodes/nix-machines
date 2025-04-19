7b30e58553ce08b6befa4ec5e081374c57ac5a70
Henrik VT <commits@henrikvt.com>
2025-04-19 16:54:40 -0400

    remove unstable option\

--- machines/nixos/moran/home/hypr.nix
+++ machines/nixos/moran/home/hypr.nix
@@ -35,7 +35,7 @@
 
     # set the Hyprland and XDPH packages to null to use the ones from the NixOS module
     package = null;
-    portalPackage = null;
+    # portalPackage = null;
 
     settings = {
       input = {
