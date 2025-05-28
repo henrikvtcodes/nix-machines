a36c1b1a9f74b8c12830ea52faa64c83234539e3
Henrik VT <commits@henrikvt.com>
2025-05-28 07:23:27 -0400

    update flake to 25.05 everything

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nix configuration for my servers + other stuff";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -12,21 +12,21 @@
     };
 
     darwin = {
-      url = "github:LnL7/nix-darwin/nix-darwin-24.11";
+      url = "github:LnL7/nix-darwin/nix-darwin-25.05";
       # inputs.nixpkgs.follows = "nixpkgs";
     };
 
     hardware.url = "github:NixOS/nixos-hardware";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       # inputs.nixpkgs.follows = "nixpkgs";
     };
 
     hyprland.url = "github:hyprwm/Hyprland";
 
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-24.11";
+      url = "github:nix-community/nixvim/nixos-25.05";
       # inputs.nixpkgs.follows = "nixpkgs";
     };
 
