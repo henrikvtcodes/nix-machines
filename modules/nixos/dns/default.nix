b50ba2e0f192bb9569533e7a731cc76f186cd7a7
Henrik VT <commits@henrikvt.com>
2025-08-08 16:27:10 -0500

    base nix and pkl config

--- modules/nixos/dns/default.nix
+++ modules/nixos/dns/default.nix
@@ -8,53 +8,62 @@
   cfg = config.my.services.dns;
 
   rootCorefile = builtins.readFile ./Corefile;
+  
 in {
   options.my.services.dns = with lib; {
     enable = mkOption {
       type = types.bool;
       default = false;
-      description = "Enable custom CoreDNS DNS server";
+      description = "Enable custom Tungsten DNS server";
+    };
+    package = mkOption {
+      type = types.package;
+      default = inputs.tungsten.packages.${system}.tungsten;
+    };
+    controlSocket = mkOption {
+      type = types.string;
+      default = "/run/tungsten/tungsten.sock";
+      description = "The control socket that allows issuing reload and other control commands";
+    };
+    configFile = mkOption {
+      type = types.path;
+      default = ./config.pkl;
+      description = "The server config file";
     };
   };
 
   config = with lib;
     mkIf cfg.enable {
-      services.coredns = {
-        enable = true;
-        package = inputs.coredns.packages.${system}.coredns;
-        config = rootCorefile;
-      };
-
       users = {
-        users.coredns = {
+        users.tungsten = {
           isSystemUser = true;
-          group = "coredns";
+          group = "tungsten";
           extraGroups = ["tailscale"];
         };
-        groups.coredns = {};
+        groups.tungsten = {};
       };
 
       networking.search = [
         "ts.unicycl.ing"
       ];
 
-      systemd.services.coredns = let
+      systemd.services.tungsten = let
         caps = [
-          "CAP_NET_ADMIN"
           "CAP_NET_BIND_SERVICE"
           "CAP_NET_RAW"
-          "CAP_SYS_ADMIN"
         ];
       in {
-        description = mkForce "Coredns DNS server";
         wants = ["tailscaled.service"];
         after = ["tailscaled.service"];
         serviceConfig = {
-          CapabilityBoundingSet = mkForce caps;
-          AmbientCapabilities = mkForce caps;
-          User = "coredns";
-          Group = "coredns";
-          DynamicUser = mkForce false;
+          CapabilityBoundingSet = caps;
+          AmbientCapabilities = caps;
+          User = "tungsten";
+          Group = "tungsten";
+          LimitNPROC = 512;
+          ProtectHome="read-only";
+          ExecStart = "${cfg.package}/bin/tungsten serve --socket=${cfg.controlSocket} --config=${cfg.configFile}";
+          ExecReload = "${cfg.package}/bin/tungsten reload --socket=${cfg.controlSocket}";
         };
         stopIfChanged = false;
       };
