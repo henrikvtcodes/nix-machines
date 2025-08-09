6ee5aaff7fbe819b527de819b0f4d220be0131f2
Henrik VT <commits@henrikvt.com>
2025-08-08 19:46:53 -0500

    ashokan: update bgptools prometheus url and add stirling pdf

--- machines/nixos/ashokan/services/metrics.nix
+++ machines/nixos/ashokan/services/metrics.nix
@@ -19,7 +19,7 @@
         scrape_interval = "1m";
         scheme = "https";
         metrics_path = "/prom/9bd7e9e4-f70a-48a9-a9a1-06396db1801a";
-        static_configs = [{targets = ["bgp.tools"];}];
+        static_configs = [{targets = ["prometheus.bgp.tools"];}];
       }
 
       {
