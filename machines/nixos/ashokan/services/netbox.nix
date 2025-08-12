5221fd793ccfcaa8f3d29534caba23510043d0a9
Henrik VT <commits@henrikvt.com>
2025-08-11 23:43:49 -0500

    serve netbox static files

--- machines/nixos/ashokan/services/netbox.nix
+++ machines/nixos/ashokan/services/netbox.nix
@@ -6,12 +6,34 @@
     secretKeyFile = config.age.secrets.netboxSecretKey.path;
   };
 
-  services.traefik.dynamicConfigOptions = {
+  services.nginx = {
+    enable = true;
+    group = "netbox";
+    virtualHosts."netbox-static" = {
+      listen = [{
+        addr = "127.0.0.1";
+        port = 9001;
+      }];
+      locations."/" = { root = "${config.services.netbox.dataDir}"; };
+    };
+  };
+
+  services.traefik.dynamicConfigOptions = let 
+  domain = "netbox.unicycl.ing";
+  in {
     http = {
       routers = {
         netbox = {
-          rule = "Host(`netbox.unicycl.ing`)";
+          rule = "Host(`${domain}`)";
           service = "netbox";
+          entryPoints = [
+            "https"
+            "http"
+          ];
+        };
+        "netbox-static" = {
+          rule = "Host(`${domain}`) && PathPrefix(`/static`)";
+          service = "netbox-static";
           entryPoints = [
             "https"
             "http"
@@ -23,6 +45,9 @@
           loadBalancer = {
             servers = [{url = "http://localhost:${toString config.services.netbox.port}";}];
           };
+        };
+        "netbox-static" = {
+          loadBalancer.servers = [{ url = "http://127.0.0.1:9001"; }];
         };
       };
     };
