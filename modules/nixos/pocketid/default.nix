5b325a5aef6664ed657df3a76e0864569fca3213
Henrik VT <commits@henrikvt.com>
2025-09-09 12:25:37 -0400

    pocket-id: copy module code from nixpkgs unstable bc it better

--- modules/nixos/pocketid/default.nix
+++ modules/nixos/pocketid/default.nix
@@ -2,9 +2,14 @@
   config,
   lib,
   unstable,
+  pkgs,
   ...
 }: let
   cfg = config.my.services.pocketid;
+  pCfg = config.services.pocket-id;
+
+  format = pkgs.formats.keyValue {};
+  settingsFile = format.generate "pocket-id-env-vars" pCfg.settings;
 in {
   options.my.services.pocketid = with lib; {
     enable = mkEnableOption {
@@ -19,7 +24,7 @@ in {
       default = 7007;
       description = "Port for PocketID";
     };
-    dataPath = mkOption {
+    dataDir = mkOption {
       type = types.str;
       default = "/var/lib/pocketid/data";
       description = "Path to store PocketID data";
@@ -53,12 +58,105 @@ in {
     # };
 
     services.pocket-id = {
-      enable = true;
+      # Using the module code copied from nixpkgs unstable but still using some stuff here.
+      enable = false;
       package = unstable.pocket-id;
+      dataDir = cfg.dataDir;
       settings = {
         APP_URL = "https://${cfg.domainName}";
         TRUST_PROXY = true;
         PORT = toString cfg.port;
+      };
+    };
+
+    systemd.tmpfiles.rules = [
+      "d ${cfg.dataDir} 0755 pocketid pocketid"
+    ];
+
+    users = {
+      users.pocket-id = {
+        isSystemUser = true;
+        group = "pocket-id";
+        description = "Pocket ID backend user";
+        home = cfg.dataDir;
+      };
+
+      groups.pocket-id = {};
+    };
+
+    systemd.services = {
+      pocket-id = {
+        description = "Pocket ID";
+        after = ["network.target"];
+        wantedBy = ["multi-user.target"];
+        restartTriggers = [
+          pCfg.package
+          pCfg.environmentFile
+          settingsFile
+        ];
+
+        serviceConfig = {
+          Type = "simple";
+          User = "pocket-id";
+          Group = "pocket-id";
+          WorkingDirectory = cfg.dataDir;
+          ExecStart = lib.getExe pCfg.package;
+          Restart = "always";
+          EnvironmentFile = [
+            pCfg.environmentFile
+            settingsFile
+          ];
+
+          # Hardening
+          AmbientCapabilities = "";
+          CapabilityBoundingSet = "";
+          DeviceAllow = "";
+          DevicePolicy = "closed";
+          #IPAddressDeny = "any"; # provides the service through network
+          LockPersonality = true;
+          MemoryDenyWriteExecute = true;
+          NoNewPrivileges = true;
+          PrivateDevices = true;
+          PrivateNetwork = false; # provides the service through network
+          PrivateTmp = true;
+          PrivateUsers = true;
+          ProcSubset = "pid";
+          ProtectClock = true;
+          ProtectControlGroups = true;
+          ProtectHome = true;
+          ProtectHostname = true;
+          ProtectKernelLogs = true;
+          ProtectKernelModules = true;
+          ProtectKernelTunables = true;
+          ProtectProc = "invisible";
+          ProtectSystem = "strict";
+          ReadWritePaths = [cfg.dataDir];
+          RemoveIPC = true;
+          RestrictAddressFamilies = [
+            "AF_UNIX"
+            "AF_INET"
+            "AF_INET6"
+          ];
+          RestrictNamespaces = true;
+          RestrictRealtime = true;
+          RestrictSUIDSGID = true;
+          SystemCallArchitectures = "native";
+          SystemCallFilter = lib.concatStringsSep " " [
+            "~"
+            "@clock"
+            "@cpu-emulation"
+            "@debug"
+            "@module"
+            "@mount"
+            "@obsolete"
+            "@privileged"
+            "@raw-io"
+            "@reboot"
+            "@resources"
+            "@swap"
+          ];
+          UMask = "0077";
+        };
       };
     };
 
