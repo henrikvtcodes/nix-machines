a87fc96ca54e21dc613b72f5ca099b6fc6d836b6
Henrik VT <commits@henrikvt.com>
2025-12-21 21:06:24 -0500

    remove X-Frame-Options header

--- modules/nixos/caddy/default.nix
+++ modules/nixos/caddy/default.nix
@@ -52,7 +52,6 @@ in
           	header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload"
           	header X-Content-Type-Options nosniff
           	header X-XSS-Protection "1; mode=block"
-          	header X-Frame-Options DENY
           	header Referrer-Policy no-referrer
           	header Permissions-Policy interest-cohort=()
           	header Cross-Origin-Embedder-Policy require-corp
