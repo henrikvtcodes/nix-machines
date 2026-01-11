9aa2af190532796284ecb9272c4bfb83ab807500
Henrik VT <commits@henrikvt.com>
2026-01-11 17:16:30 -0500

    inline some stuff

--- machines/nixos/moran/home/default.nix
+++ machines/nixos/moran/home/default.nix
@@ -30,7 +30,9 @@
       user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICM+1ip8IBO+sK8J7cOwEtA/ba+tTtPHUGYC/KW6mppU";
       gpg.format = "ssh";
       gpg.ssh.program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
-      gpg.ssh.allowedSignersFile = toString ./signers.txt;
+      gpg.ssh.allowedSignersFile = ''
+        commits@henrikvt.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICM+1ip8IBO+sK8J7cOwEtA/ba+tTtPHUGYC/KW6mppU
+      '';
       commit.gpgsign = true;
     };
     lazygit.settings = {
