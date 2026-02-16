a4f141e68b376cedb98f274fb04ea5c1a72d796f
Henrik VT <commits@henrikvt.com>
2026-02-15 22:29:33 -0500

    trying again

--- machines/nixos/mci/hardware-config.nix
+++ machines/nixos/mci/hardware-config.nix
@@ -13,18 +13,18 @@
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/7e687e96-7a58-4cf2-9cc8-c832bb541abe";
+    device = "/dev/disk/by-uuid/9cbb81bd-f16b-44a0-b8de-f3c45907cd61";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/80E1-BDA8";
+    device = "/dev/disk/by-uuid/EB58-60ED";
     fsType = "vfat";
     options = ["fmask=0022" "dmask=0022"];
   };
 
   swapDevices = [];
 
-  system.stateVersion = "25.11";
+  system.stateVersion = "24.11";
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
