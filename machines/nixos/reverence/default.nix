7c1498b169c0b703fc1a991370c9adbb084c3325
Henrik VT <commits@henrikvt.com>
2026-06-07 16:37:44 -0400

    new machine

---
+++ machines/nixos/reverence/default.nix
@@ -0,0 +1,87 @@
+{...}: {
+  imports = [
+    ./hardware-config.nix
+    # ./routing
+  ];
+
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+  };
+
+  virtualisation.podman.enable = true;
+
+  home.henrikvt.enable = true;
+  users.henrikvt.enablePasswordFile = false;
+
+  my.services = {
+    tailscale = {
+      enable = false;
+      advertiseExitNode = false;
+    };
+    caddy.enable = false;
+  };
+
+  services = {
+    qemuGuest.enable = true;
+    openssh = {
+      openFirewall = false;
+      ports = [22 69];
+    };
+  };
+
+  users.users.henrikvt.extraGroups = ["bird" "knot" "pcap"];
+
+  programs.tcpdump.enable = true;
+
+  systemd.network = {
+    links = {
+      "10-mgmt" = {
+        matchConfig = {
+          MACAddress = "BC:24:11:21:8A:57";
+          Type = "ether";
+        };
+        linkConfig = {
+          Name = "nic0";
+        };
+      };
+      "20-ix" = {
+        matchConfig = {
+          MACAddress = "38:2C:DB:06:34:47";
+          Type = "ether";
+        };
+        linkConfig = {
+          Name = "nic1";
+        };
+      };
+    };
+    networks = {
+      "10-mgmt" = {
+        matchConfig = {Name = "nic0";};
+        networkConfig = {
+          Description = "Backend Management NIC";
+          DHCP = "yes";
+        };
+      };
+      "20-ix" = {
+        matchConfig = {Name = "nic1";};
+        networkConfig = {
+          Description = "Vermont IX Peering Lan";
+          DHCP = "no";
+          IPv6AcceptRA = "no";
+          IPv6SendRA = "no";
+          EmitLLDP = "no";
+        };
+      };
+    };
+  };
+
+  networking = {
+    hostName = "reverence";
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [22 69 80 443 2023 8080];
+      allowedUDPPorts = [80 443];
+    };
+  };
+}
