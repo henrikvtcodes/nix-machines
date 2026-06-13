418546e293d50a5a5381378f6089e9d5e9216c51
Henrik VT <commits@henrikvt.com>
2026-06-13 17:16:15 -0400

    working mail stuff

--- machines/nixos/mci/mail.nix
+++ machines/nixos/mci/mail.nix
@@ -5,14 +5,37 @@ in {
     postfix = {
       enable = true;
       settings.main = {
+        # Identity: HELO/EHLO as an FQDN that matches the PTR of the
+        # sending IPs (mx.mci.as63477.net -> .25, both v4 and v6).
+        myhostname = "mx.mci.as63477.net";
+        # Locally-generated mail (bounces, root/cron) gets a sender domain
+        # that has SPF coverage.
+        myorigin = "lists.as63477.net";
+
+        # Always egress from the dedicated mail IPs.
         smtp_bind_address = "155.103.251.25";
         smtp_bind_address6 = "2602:f542:bee::25";
         smtp_bind_address_enforce = "yes";
-        smtp_address_preference = "ipv6";
+        # "any" lets Postfix fall back to IPv4 when a remote's IPv6 :25 is
+        # unreachable, instead of hard-failing on an IPv6-only attempt.
+        smtp_address_preference = "any";
+
+        # DKIM signing via opendkim milter (inet socket below).
+        milter_default_action = "accept";
+        smtpd_milters = ["inet:localhost:8891"];
+        non_smtpd_milters = ["inet:localhost:8891"];
+
         transport_maps = ["hash:/var/lib/mailman/data/postfix_lmtp"];
         local_recipient_maps = ["hash:/var/lib/mailman/data/postfix_lmtp"];
         relay_domains = ["hash:/var/lib/mailman/data/postfix_domains"];
       };
+    };
+    opendkim = {
+      enable = true;
+      selector = "mail";
+      domains = "csl:lists.as63477.net";
+      # inet socket avoids unix-socket permission/chroot issues with Postfix.
+      socket = "inet:8891@localhost";
     };
     mailman = {
       enable = true;
