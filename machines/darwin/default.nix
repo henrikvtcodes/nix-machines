ea2a47706db0e6a511057003af0f5ab82aec7cde
henrikvtcodes <commits@henrikvt.com>
2026-09-26 17:29:39 -0400

    move settings

--- machines/darwin/default.nix
+++ machines/darwin/default.nix
@@ -2,43 +2,13 @@
   pkgs,
   lib,
   ...
-}: let
-  launchdWeekly = {
-    Hour = 3;
-    Minute = 0;
-    Weekday = 0;
-  };
-in {
+}: {
   imports = [
     ../../modules/darwin
     ../../home/henrikvt
   ];
 
-  # Clean up nix store + old generations automatically
-  nix = lib.mkDefault {
-    package = pkgs.nix;
-    gc = lib.mkDefault {
-      automatic = true;
-      interval = launchdWeekly;
-      options = "--delete-older-than 7d";
-    };
-    optimise = lib.mkDefault {
-      automatic = true;
-      interval = launchdWeekly;
-    };
-    settings = {
-      experimental-features = [
-        "nix-command"
-        "flakes"
-        "recursive-nix"
-      ];
-      system-features = ["recursive-nix"];
-      extra-substituters = ["https://nix-community.cachix.org"];
-      extra-trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-    };
-  };
+
   nixpkgs.config.allowUnfree = true;
 
   # TouchID Prompt for sudo
--- machines/darwin/pepacton/default.nix
+++ machines/darwin/pepacton/default.nix
@@ -4,7 +4,39 @@
   pkgs,
   lib,
   ...
-}: {
+}:  let
+  launchdWeekly = {
+    Hour = 3;
+    Minute = 0;
+    Weekday = 0;
+  };
+in  {
+    # Clean up nix store + old generations automatically
+  nix = {
+    package = pkgs.nix;
+    gc = {
+      automatic = true;
+      interval = launchdWeekly;
+      options = "--delete-older-than 7d";
+    };
+    optimise = lib.mkDefault {
+      automatic = true;
+      interval = launchdWeekly;
+    };
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "recursive-nix"
+      ];
+      system-features = ["recursive-nix"];
+      extra-substituters = ["https://nix-community.cachix.org"];
+      extra-trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+  };
+
   system.primaryUser = "henrikvt";
   users.users.henrikvt = {
     home = "/Users/henrikvt";
